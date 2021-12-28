da9e7eef05aed14e1b2ba98a8385ecaf4ae129b4
Gabriela SD <gabriela.deamatis@gmail.com>
2021-12-28 18:53:02 -0300

    primeiro commit

---
+++ main.js
@@ -0,0 +1,67 @@
+/*abre e fecha o menu*/
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+/*clicando no item do menu e direcionando para a seção clicada*/
+const links = document.querySelectorAll('nav ul li a')
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/*scroll rolagem de página*/
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+function changeHeaderWhenScroll() {
+  if (window.scrollY >= navHeight) {
+    // scroll é maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    // menor que a altura do header
+    header.classList.remove('scroll')
+  }
+}
+
+/*mostrar os elementos da página quando rolar */
+const scrollReveal = ScrollReveal({
+  origin: 'top',
+  distance: '30px',
+  duration: 500,
+  reset: true
+})
+scrollReveal.reveal(
+  `#home .txtAbertura, #home .imageAbertura,
+  #about h3, #about .txtAbout
+  #galeria h3, #galeria .card,
+  #services h3, #services p,
+  #contact h3, #contact .grid
+  `,
+  {
+    interval: 100
+  }
+)
+/* Botão voltar para o topo */
+const backToTopButton = document.querySelector('.back-to-top')
+
+function backToTop() {
+  console.log(window.scrollY)
+  if (window.scrollY >= 560) {
+    backToTopButton.classList.add('show')
+  } else {
+    backToTopButton.classList.remove('show')
+  }
+}
+
+/* When Scroll */
+window.addEventListener('scroll', function () {
+  changeHeaderWhenScroll()
+  backToTop()
+  activateMenuAtCurrentSection()
+})
